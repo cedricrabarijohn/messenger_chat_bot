f57df800e715c2d5bf7310f16cffa0abb08f7a66
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-24 21:11:42 +0300

    Changing env file

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 // console.log(process.env.ACCESS_TOKEN)
 const bot = new BootBot({
-  accessToken: process.env.ACCESS_TOKEN,
-  verifyToken: process.env.VERIFY_TOKEN,
-  appSecret: process.env.APP_SECRET
+  accessToken: process.env.VERCEL_ACCESS_TOKEN,
+  verifyToken: process.env.VERCEL_VERIFY_TOKEN,
+  appSecret: process.env.VERCEL_APP_SECRET
 });
- 
+
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
   chat.say(`Echo: ${text}`);
